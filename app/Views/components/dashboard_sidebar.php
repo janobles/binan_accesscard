872530cd9b2316aab929edd040530d6a31c0229f
jp-gerona <jp-gerona@protonmail.com>
2026-07-01 15:27:38 +0800

    fix(scanner): rename sidebar section heading to 'Scanner'

--- app/Views/components/dashboard_sidebar.php
+++ app/Views/components/dashboard_sidebar.php
@@ -74,7 +74,7 @@ $activeTab = $activeTab ?? '';
             <a class="nav-link <?= esc($navActive['cards'] ?? '') ?>" href="<?= site_url('admin/cards') ?>"><i class="bi bi-qr-code" aria-hidden="true"></i><span>QR Cards</span></a>
         </li>
         <li><hr class="sidebar-divider"></li>
-        <li><div class="sidebar-heading">Aid Distribution</div></li>
+        <li><div class="sidebar-heading">Scanner</div></li>
         <li class="nav-item">
             <a class="nav-link <?= esc($navActive['scanner'] ?? '') ?>" href="<?= site_url('scanner/scan') ?>"><i class="bi bi-upc-scan" aria-hidden="true"></i><span>Scanner</span></a>
         </li>
